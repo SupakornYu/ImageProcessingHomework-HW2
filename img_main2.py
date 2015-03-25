927249b86c141e948906d1b049bfeca2cc73a735
supakorn <supakorn_pump@hotmail.com>
2015-03-26 03:19:19 +0700

    -shift pic in fourier is done -rotate pic is done

--- img_main2.py
+++ img_main2.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import cmath
 
 def padwithzeros(vector, pad_width, iaxis, kwargs):
     vector[:pad_width[0]] = 255
@@ -76,25 +77,74 @@ class ImageLibFourier:
     def pgmDataToPhasePicWithScale(self,filename,pgmDataFourier,pgmSize,pgmGreyscale):
         #pgmDataFourier = np.log(np.angle(pgmDataFourier))
         pgmDataFourier = np.angle(pgmDataFourier)
+        #pgmDataFourier = 1+np.log2(1+pgmDataFourier)
         pgmDataFourier = np.around(pgmDataFourier*(float(pgmGreyscale[0])/(np.amax(pgmDataFourier)-np.amin(pgmDataFourier))),0).astype(int)
         #pgmDataFourierPhase = np.around(np.angle(pgmDataFourier)*(16),0).astype(int)
         self.buildPGMFile(filename+"phase",pgmSize[0],pgmSize[1],pgmGreyscale,pgmDataFourier)
-        print pgmDataFourier
-        return True
 
+    def pgmDataToAmplitudePicWithScale(self,filename,pgmDataFourier,pgmSize,pgmGreyscale):
+        pgmDataFourier = np.around(np.abs(pgmDataFourier/float(pgmGreyscale[0])),0).astype(int)
+        #pgmDataFourier = np.around(1+np.log2(1+np.abs(pgmDataFourier)),0).astype(int)
+        self.buildPGMFile(filename+"Amplitude",pgmSize[0],pgmSize[1],pgmGreyscale,pgmDataFourier)
+        #np.set_printoptions(threshold=np.nan)
 
-#np.set_printoptions(threshold=np.nan)
-myLib = ImageLibFourier()
-myLib.padImage("Cross.pgm",28)
-pgmVer,pgmComment,pgmSize,pgmGreyscale,pgmData,htg = myLib.readPGMImage("CrossPadding.pgm")
-pgmData = myLib.moveAxispgmDataBeforeFourier(pgmData,pgmSize)
-pgmDataFourier = myLib.convertToFourier(pgmData)
-print pgmDataFourier
-myLib.pgmDataToPhasePicWithScale("CrossPadding",pgmDataFourier,pgmSize,pgmGreyscale)
+    def shiftAxisInFourier(self,filename,a,b):
+        pgmVer,pgmComment,pgmSize,pgmGreyscale,pgmData,htg = self.readPGMImage(filename)
+        #pgmData = myLib.moveAxispgmDataBeforeFourier(pgmData,pgmSize)
+        pgmDataFourier = self.convertToFourier(pgmData)
+        #print pgmDataFourier
+        for j in range(int(pgmSize[1])):
+            for i in range(int(pgmSize[0])):
+                #realSim = np.cos(-2*np.pi*(((a*i)/int(pgmSize[0]))+((b*j)/int(pgmSize[1]))))
+                #complexSim = np.sin(-2*np.pi*(((a*i)/int(pgmSize[0]))+((b*j)/int(pgmSize[1]))))*1j
+                #pgmDataFourier[i][j] = (realSim+complexSim)* pgmDataFourier[i][j]
+                pgmDataFourier[i][j] = pgmDataFourier[i][j] * cmath.exp(-2j*np.pi*((a*i/float(pgmSize[0]))+(b*j/float(pgmSize[1]))))
+        pgmDataInverse = np.around(np.abs(np.fft.ifft2(pgmDataFourier)),0).astype(int)
+        #print pgmDataInverse
+        filename = filename.replace(".pgm", "");
+        self.buildPGMFile(filename+"ShiftAxis",pgmSize[0],pgmSize[1],pgmGreyscale,pgmDataInverse)
 
-pgmDataFourierDivideSampleAmp =  np.around(np.abs(pgmDataFourier)/(256),0).astype(int)
-print pgmDataFourierDivideSampleAmp
-print pgmDataFourierPhase
+    def rotatePic(self,filename,angle):
+        angle = np.deg2rad(angle)
+        pgmVer,pgmComment,pgmSize,pgmGreyscale,pgmData,htg = self.readPGMImage(filename)
+        pgmDataNew  = np.full((int(pgmSize[0]), int(pgmSize[0])), 255, dtype=np.int)
 
-myLib.buildPGMFile("amplitude",pgmSize[0],pgmSize[1],pgmGreyscale,pgmDataFourierDivideSampleAmp)
-print htg
+        for j in range(int(pgmSize[1])):
+            for i in range(int(pgmSize[0])):
+                xAxis = (i-100)*np.cos(angle)-(j-100)*np.sin(angle)
+                yAxis = (i-100)*np.sin(angle)+(j-100)*np.cos(angle)
+                xAxis = xAxis + 100
+                yAxis = yAxis + 100
+                #print xAxis
+                #print yAxis
+                if xAxis >= 200 or yAxis >= 200 or xAxis < 0 or yAxis < 0:
+                    pgmDataNew[i][j] = 255
+                else:
+                    pgmDataNew[i][j] = pgmData[xAxis][yAxis]
+        filename = filename.replace(".pgm", "");
+        self.buildPGMFile(filename+"RotatePic",pgmSize[0],pgmSize[1],pgmGreyscale,pgmDataNew)
+        #print pgmDataNew
+
+if __name__ == "__main__":
+
+    """
+    #1.1
+    myLib = ImageLibFourier()
+    myLib.padImage("Cross.pgm",28)
+    pgmVer,pgmComment,pgmSize,pgmGreyscale,pgmData,htg = myLib.readPGMImage("CrossPadding.pgm")
+    pgmData = myLib.moveAxispgmDataBeforeFourier(pgmData,pgmSize)
+    pgmDataFourier = myLib.convertToFourier(pgmData)
+    myLib.pgmDataToPhasePicWithScale("CrossPadding",pgmDataFourier,pgmSize,pgmGreyscale)
+    myLib.pgmDataToAmplitudePicWithScale("CrossPadding",pgmDataFourier,pgmSize,pgmGreyscale)
+    """
+
+    """
+    #1.2
+    myLib = ImageLibFourier()
+    myLib.padImage("Cross.pgm",28)
+    myLib.shiftAxisInFourier("CrossPadding.pgm",20,30)
+    """
+
+    #1.3
+    myLib = ImageLibFourier()
+    myLib.rotatePic("Cross.pgm",-30)
